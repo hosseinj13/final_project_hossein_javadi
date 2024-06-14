bc3d15377050400703f5f20985e19f673a03ec6e
hossein javadi <hosseinj13@yahoo.com>
2024-06-14 16:21:43 +0330

    Create InformationDuplicateException.java.

---
+++ src/main/java/exception/InformationDuplicateException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class InformationDuplicateException extends RuntimeException{
+
+    public InformationDuplicateException(String message) {
+        super(message);
+    }
+}
