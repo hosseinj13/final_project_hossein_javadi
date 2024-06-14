a08d6719e40b3b09feb81909a9990e20f6e1adec
hossein javadi <hosseinj13@yahoo.com>
2024-06-14 16:44:53 +0330

    Create CommentService.java And define methods.

--- src/main/java/service/comment/CommentService.java
+++ src/main/java/service/comment/CommentService.java
@@ -10,6 +10,5 @@ import java.util.List;
 public interface CommentService extends BaseService<Comment, Long> {
 
     List<Comment> findAllByCustomer(Customer customer);
-
     void deleteComment(Comment selectedComment);
 }
