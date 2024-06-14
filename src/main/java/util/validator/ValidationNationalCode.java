85b17c85fd051c7a95d34c785a794c1fe73bef3d
hossein javadi <hosseinj13@yahoo.com>
2024-06-14 16:52:46 +0330

    Create ValidationNationalCode.java

---
+++ src/main/java/util/validator/ValidationNationalCode.java
@@ -0,0 +1,16 @@
+package util.validator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = ValidNationalCode.class)
+@Target( { ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidationNationalCode {
+    String message() default "Invalid nation code";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
